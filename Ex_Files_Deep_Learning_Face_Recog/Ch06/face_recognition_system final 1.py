e83f4497d377405fc5953c0d2e70eca82a93d73e
Prav <kappakpr@gmail.com>
2020-07-06 23:03:31 -0700

    multiple faces in a image

--- Ex_Files_Deep_Learning_Face_Recog/Ch06/face_recognition_system final 1.py
+++ Ex_Files_Deep_Learning_Face_Recog/Ch06/face_recognition_system final 1.py
@@ -1,4 +1,6 @@
 #loops through all the .jpg files in the directory and checks against person_1, person_2, person-3
+#unknown_5.jpg also has three faces, but algorithm could identify only persons 1 & 3.
+
 import face_recognition
 import os
 
@@ -24,17 +26,26 @@ directory = r'.'
 for unknown_file in os.listdir(directory):
     # .jpg filter
     if unknown_file.endswith(".jpg"):
+        print(f"Checking {unknown_file}")
         unknown_image = face_recognition.load_image_file(unknown_file)
 
         # Get face encodings for any people in the picture
         unknown_face_encodings = face_recognition.face_encodings(unknown_image)
 
+        # print(f" Length : {len(unknown_face_encodings)}")
+        if len(unknown_face_encodings) > 1:
+            print(f"{unknown_file} has multiple faces!!!!")
+
         # There might be more than one person in the photo, so we need to loop over each face we found
         for unknown_face_encoding in unknown_face_encodings:
 
-        # Test if this unknown face encoding matches any of the three people we know
+            # Test if this unknown face encoding matches any of the three people we know
             results = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding, tolerance=0.6)
 
+
+
+            #print(f" Length : {len(unknown_face_encoding)}")
+            #print(f"{results[0]}")
             name = unknown_file
 
             if results[0]:
