2882f505a84aedfe7f4eba40146b6d9f45140f11
Prav <kappakpr@gmail.com>
2020-07-14 23:49:42 -0700

    initial

--- SentimentAnalysis/Senti1.py
+++ SentimentAnalysis/Senti1.py
@@ -19,7 +19,7 @@ print(f"info :- {info}")
 #print(f"length of the dataset {len(list(train_dataset))}")
 
 BUFFER_SIZE = 300
-BATCH_SIZE = 10
+BATCH_SIZE = 100
 
 #shuffle
 print("shuffle the data to make sure training data set is a good mix, small batch size can skew the training data")
@@ -58,3 +58,53 @@ for reviews in train_dataset.take(5):
     print(review_text.get('review_body').numpy())
     encoded_example = encoder.encode(review_text.get('review_body').numpy())
     print(encoded_example)
+
+for index in encoded_example:
+    print('{} ----> {}'.format(index,encoder.decode([index])))
+
+def encode(text_tensor, label_tensor):
+    encoded_text = encoder.encode(text_tensor.numpy())
+    label = tf.where(label_tensor>3,1,0)
+    return encoded_text, label
+
+def encode_map_fn(tensor):
+    text = tensor['data'].get('review_body')
+    label = tensor['data'].get('star_rating')
+
+    encoded_text, label = tf.py_function(encode,
+                                         inp=[text,label],
+                                         Tout=(tf.int64,tf.int32))
+    encoded_text.set_shape([None])
+    label.set_shape([])
+
+    return encoded_text, label
+
+ar_encoded_data = train_dataset.map(encode_map_fn)
+
+for f0,f1 in ar_encoded_data.take(5):
+    print(f"f0 encoded review body:- {f0}")
+    print(f"f1 encoded star rating:- {f1}")
+
+TAKE_SIZE = 300
+
+# skip the number of records specified in TAKE_SIZE and assign remaining to train data
+# text data will be variable length, pad
+
+train_data = ar_encoded_data.skip(TAKE_SIZE).shuffle(BUFFER_SIZE)
+train_data = train_data.padded_batch(BATCH_SIZE)
+
+test_data = ar_encoded_data.take(TAKE_SIZE)
+test_data = test_data.padded_batch(BATCH_SIZE)
+
+vocab_size += 1
+
+sample_text, sample_labels = next(iter(test_data))
+
+print(sample_text[0])
+print(sample_labels[0])
+
+print(sample_text[1])
+print(sample_labels[1])
+
+for f0,f1 in test_data.take(10):
+    print(tf.unique_with_counts(f1)[2].numpy())
