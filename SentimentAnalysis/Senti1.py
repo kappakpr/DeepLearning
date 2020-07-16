7ebaa10de71a472270ad107dd8de859effcff9cf
Prav <kappakpr@gmail.com>
2020-07-16 10:01:42 -0700

    initial

--- SentimentAnalysis/Senti1.py
+++ SentimentAnalysis/Senti1.py
@@ -12,13 +12,13 @@ print(tf.config.experimental.list_physical_devices())
 dataset, info = tfds.load('amazon_us_reviews/Mobile_Electronics_v1_00',with_info=True)
 train_dataset=dataset['train']
 
-print(f"train_dataset :- {train_dataset}")
+#print(f"train_dataset :- {train_dataset}")
 
-print(f"info :- {info}")
+#print(f"info :- {info}")
 
 #print(f"length of the dataset {len(list(train_dataset))}")
 
-BUFFER_SIZE = 30000
+BUFFER_SIZE = 100
 BATCH_SIZE = 128
 
 #shuffle
@@ -26,14 +26,14 @@ print("shuffle the data to make sure training data set is a good mix, small batc
 train_dataset = train_dataset.shuffle(BUFFER_SIZE, reshuffle_each_iteration=False)
 
 #print 5 records, iterate through the loop take 5 rows
-for reviews in train_dataset.take(5):
-    print(reviews)
+#for reviews in train_dataset.take(5):
+#    print(reviews)
 
 for reviews in train_dataset.take(20):
     review_text = reviews['data']
-    print(review_text.get('review_body').numpy())
-    print(review_text.get('star_rating'))
-    print(tf.where(review_text.get('star_rating')>3,1,0).numpy())
+#    print(review_text.get('review_body').numpy())
+#    print(review_text.get('star_rating'))
+#    print(tf.where(review_text.get('star_rating')>3,1,0).numpy())
 
 tokenizer = tfds.features.text.Tokenizer()
 
@@ -55,12 +55,12 @@ encoder = tfds.features.text.TokenTextEncoder(vocabulary_set)
 
 for reviews in train_dataset.take(5):
     review_text = reviews['data']
-    print(review_text.get('review_body').numpy())
+    #print(review_text.get('review_body').numpy())
     encoded_example = encoder.encode(review_text.get('review_body').numpy())
-    print(encoded_example)
+    #print(encoded_example)
 
-for index in encoded_example:
-    print('{} ----> {}'.format(index,encoder.decode([index])))
+#for index in encoded_example:
+    #print('{} ----> {}'.format(index,encoder.decode([index])))
 
 def encode(text_tensor, label_tensor):
     encoded_text = encoder.encode(text_tensor.numpy())
@@ -81,11 +81,11 @@ def encode_map_fn(tensor):
 
 ar_encoded_data = train_dataset.map(encode_map_fn)
 
-for f0,f1 in ar_encoded_data.take(5):
-    print(f"f0 encoded review body:- {f0}")
-    print(f"f1 encoded star rating:- {f1}")
+#for f0,f1 in ar_encoded_data.take(5):
+#    print(f"f0 encoded review body:- {f0}")
+#    print(f"f1 encoded star rating:- {f1}")
 
-TAKE_SIZE = 300
+TAKE_SIZE = 100
 
 # skip the number of records specified in TAKE_SIZE and assign remaining to train data
 # text data will be variable length, pad
@@ -97,24 +97,24 @@ test_data = ar_encoded_data.take(TAKE_SIZE)
 test_data = test_data.padded_batch(BATCH_SIZE)
 
 vocab_size += 1
-
+print(f"vocab_size increased {vocab_size}")
 sample_text, sample_labels = next(iter(test_data))
 
-print(sample_text[0])
-print(sample_labels[0])
+#print(sample_text[0])
+#print(sample_labels[0])
 
-print(sample_text[1])
-print(sample_labels[1])
+#print(sample_text[1])
+#print(sample_labels[1])
 
-for f0,f1 in test_data.take(10):
-    print(tf.unique_with_counts(f1)[2].numpy())
+#for f0,f1 in test_data.take(10):
+#    print(tf.unique_with_counts(f1)[2].numpy())
 
 print("model define started")
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Embedding(vocab_size,128))
-model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,return_sequences=True)))
-model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)))
-for units in [64,64]:
+model.add(tf.keras.layers.Embedding(vocab_size + 1000,128))  # was getting an arrayoutbound kind of exception, so increased vocab size by 1000 randomly
+#model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128,return_sequences=True)))
+#model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)))
+for units in [64]: #,64]:
     model.add(tf.keras.layers.Dense(units,activation='relu'))
 model.add(tf.keras.layers.Dense(1))
 
